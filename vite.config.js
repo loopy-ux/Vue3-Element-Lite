4e698e029dc4d2c1bebd46801eeb5ba005a4ee7a
loopy-ux <3352905048@qq.com>
2025-04-03 01:30:35 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'ElementSimple',
+      fileName: (format) => `index.${format === 'es' ? 'mjs' : 'js'}`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+}) 
